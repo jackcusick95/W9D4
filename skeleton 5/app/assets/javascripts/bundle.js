1493dabaa81ab4c3757f3a8d88d99cb0d8474238
jackcusick95 <jackcusick95@gmail.com>
2021-04-22 17:29:55 -0400

    added handleclick method

--- skeleton 5/app/assets/javascripts/bundle.js
+++ skeleton 5/app/assets/javascripts/bundle.js
@@ -5,9 +5,56 @@
 /*!***********************************!*\
   !*** ./frontend/follow_toggle.js ***!
   \***********************************/
-/***/ (() => {
+/***/ ((module) => {
 
-throw new Error("Module parse failed: Unexpected token (13:9)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n| }\n| \n> render() {\n|     if (this.followState === \"followed\") {\n|         this.$el.html(\"Follow!\");");
+// const { ids } = require("webpack");
+
+class FollowToggle {
+    constructor($el) {
+        this.$el = $($el);
+        this.userId = this.$el.data("user-id");
+        this.followState = this.$el.data("initial-follow-state");
+
+        this.render();
+        this.handleClick();  
+    }
+    
+    render() {
+        if (this.followState === "followed") {
+            this.$el.html("Unfollow!");
+        } else {
+            this.$el.html("Follow!");
+        }
+    };
+
+    handleClick () {
+        this.$el.on('click', e => {
+        e.preventDefault();
+        const test = this.userId;
+        if (this.followState === "followed") {
+            $.ajax({
+                method: "DELETE",
+                url: `/users/${test}/follow`,
+                datatype: "json",
+                success: response => console.log("user unfollowed!")
+            });
+                this.followState = "unfollowed";
+                this.render();
+        } else if (this.followState === "unfollowed") {
+            $.ajax({
+                method: "POST",
+                url: `/users/${test}/follow`,
+                datatype: "json",
+                success: response => console.log("user followed!") 
+            });
+            this.followState = "followed";
+            this.render(); 
+        }
+      }); 
+    }
+    
+}
+module.exports = FollowToggle; 
 
 /***/ })
 
--- skeleton 5/frontend/follow_toggle.js
+++ skeleton 5/frontend/follow_toggle.js
@@ -6,16 +6,43 @@ class FollowToggle {
         this.userId = this.$el.data("user-id");
         this.followState = this.$el.data("initial-follow-state");
 
-        this.render(); 
+        this.render();
+        this.handleClick();  
     }
+    
+    render() {
+        if (this.followState === "followed") {
+            this.$el.html("Unfollow!");
+        } else {
+            this.$el.html("Follow!");
+        }
+    };
+
+    handleClick () {
+        this.$el.on('click', e => {
+        e.preventDefault();
+        const test = this.userId;
+        if (this.followState === "followed") {
+            $.ajax({
+                method: "DELETE",
+                url: `/users/${test}/follow`,
+                datatype: "json",
+                success: response => console.log("user unfollowed!")
+            });
+                this.followState = "unfollowed";
+                this.render();
+        } else if (this.followState === "unfollowed") {
+            $.ajax({
+                method: "POST",
+                url: `/users/${test}/follow`,
+                datatype: "json",
+                success: response => console.log("user followed!") 
+            });
+            this.followState = "followed";
+            this.render(); 
+        }
+      }); 
+    }
+    
 }
-
-render() {
-    if (this.followState === "followed") {
-        this.$el.html("Follow!");
-    } else {
-        this.$el.html("Unfollow!");
-    }
-};
-
 module.exports = FollowToggle; 
